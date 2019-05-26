78d01cb6f3f4e6fe0f7ff13ea60aaf5dab3a0b9a
Eduardo Borges <euduardoborges@gmail.com>
2019-05-26 19:04:07 -0300

    Navbar bem Stonezinha

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,6 +5,15 @@ function Navbar() {
   return (
     <nav className="navbar is-primary">
       <div className="container">
+        <div className="navbar-brand">
+          <div className="navbar-item">
+            <img
+              src="https://www.stone.com.br/wp-content/themes/stone/assets/images/stoneco.png"
+              width="120"
+              alt=""
+            />
+          </div>
+        </div>
         <div className="navbar-menu">
           <div className="navbar-end">
             <NavLink className="navbar-item" to="/wallets" activeClassName="is-active">
