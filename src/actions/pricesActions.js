9d1d6183d139aa181ceb1a2e8bc7d9fc230ad475
Eduardo Borges <euduardoborges@gmail.com>
2019-05-25 23:43:07 -0300

    Refact das actions de preco

---
+++ src/actions/pricesActions.js
@@ -0,0 +1,29 @@
+// @flow
+
+import { PricesType, StoreType, CurrencyType } from "../types";
+import { prices } from "../services/britaService";
+
+const actions = ({ getState, setState }) => ({
+  /**
+   *
+   *
+   */
+  async getBrtPrice({ PRICES: PricesType }): StoreType {
+
+    setState({ PRICES: { ...PRICES., brt: { ...PRICES.brt, isLoading: true } } });
+
+    const prices: CurrencyType = await prices();
+
+    return const out: StoreType = {
+      PRICES: {
+        ...PRICES,
+        brt: {
+          data: prices,
+          isLoading: false
+        }
+      }
+    };
+  }
+});
+
+export default actions;
