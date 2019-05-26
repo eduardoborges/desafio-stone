04094af8860b2e09bc2e044e993b5c694e982ec0
Eduardo Borges <euduardoborges@gmail.com>
2019-05-26 20:29:07 -0300

    Tela nova carteira integrada a action

--- src/screens/wallet-create/index.js
+++ src/screens/wallet-create/index.js
@@ -1,8 +1,19 @@
-import React from "react";
-import { connect } from 'unistore/react';
+import React, { useState } from "react";
+import { WalletType } from "../../types";
+import { connect } from "unistore/react";
+import actions from "../../actions";
 
 function WalletCreateScreen(props) {
+  const blankForm: WalletType = {
+    name: "Teste",
+    type: "btc"
+  };
+  const [form, setForm] = useState(blankForm);
+
+  const handleSubmit = () => props.createWallet(form);
+
   const close = () => props.history.push("/wallets");
+
   return (
     <div class="modal is-active">
       <div class="modal-background" onClick={close} />
@@ -11,9 +22,34 @@ function WalletCreateScreen(props) {
           <p class="modal-card-title">Nova Carteira</p>
           <button class="delete" aria-label="close" onClick={close} />
         </header>
-        <section class="modal-card-body">...</section>
+        <section class="modal-card-body">
+          <div className="columns is-multiline">
+            <div className="column is-6">
+              <input
+                type="text"
+                className="input"
+                value={form.name}
+                onChange={e => setForm({ ...form, name: e.target.value })}
+              />
+            </div>
+            <div className="column is-6">
+              <div class="select is-primary is-fullwidth">
+                <select
+                  value={form.type}
+                  defaultValue={form.type}
+                  onChange={e => setForm({ ...form, type: e.target.value })}
+                >
+                  <option value="btc">BTC</option>
+                  <option value="brt">BRT</option>
+                </select>
+              </div>
+            </div>
+          </div>
+        </section>
         <footer class="modal-card-foot">
-          <button class="button is-success">Criar nova carteira</button>
+          <button onClick={handleSubmit} class="button is-success">
+            Criar nova carteira
+          </button>
           <button class="button" onClick={close}>
             Cancelar
           </button>
@@ -23,4 +59,4 @@ function WalletCreateScreen(props) {
   );
 }
 
-export default WalletCreateScreen;
+export default connect('',actions)(WalletCreateScreen);
