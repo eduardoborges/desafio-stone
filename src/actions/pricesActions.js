83f5c0caac7909921c101706f7fb6654f532daa4
Eduardo Borges <euduardoborges@gmail.com>
2019-05-26 18:45:21 -0300

    Correcao nas acoes de preços

--- src/actions/pricesActions.js
+++ src/actions/pricesActions.js
@@ -1,46 +1,51 @@
 // @flow
 
 import { PricesType, StoreType, CurrencyType } from "../types";
-import { prices } from "../services/britaService";
+import { prices as brtPrices } from "../services/brtService";
+import { prices as btcPrices } from "../services/brtService";
 
 const actions = ({ getState, setState }) => ({
   /**
    *
    *
    */
-  async getBrtPrice({ PRICES: PricesType }): StoreType {
-    setState({ PRICES: { ...PRICES., brt: { ...PRICES.brt, isLoading: true } } });
-    const prices: CurrencyType = await prices();
-    return const out: StoreType = {
+  async getBrtPrice(state: StoreType): StoreType {
+    const { PRICES } = state;
+    await setState({ PRICES: { ...PRICES, brt: { ...PRICES.brt, isLoading: true } } });
+    const prices: CurrencyType = await brtPrices();
+    const out: StoreType = {
       PRICES: {
         ...PRICES,
         brt: {
           data: prices,
-          isLoading: false
+          isLoading: false,
+          isFeched: true
         }
       }
     };
-  }
+    return out;
+  },
 
   /**
-   * 
-   * 
+   *
+   *
    */
-  async getBtcPrice({ PRICES: PricesType }): StoreType {
-    setState({ PRICES: { ...PRICES., btc: { ...PRICES.btc, isLoading: true } } });
-    const prices: CurrencyType = await prices();
-    return const out: StoreType = {
+  async getBtcPrice(state: StoreType): StoreType {
+    const { PRICES } = state;
+    await setState({ PRICES: { ...PRICES, btc: { ...PRICES.btc, isLoading: true } } });
+    const prices: CurrencyType = await btcPrices();
+    const out: StoreType = {
       PRICES: {
         ...PRICES,
         btc: {
           data: prices,
-          isLoading: false
+          isLoading: false,
+          isFeched: true
         }
       }
     };
+    return out;
   }
-
-
 });
 
 export default actions;
