12aa9b47b18d56accb27a79add5928794560bcd2
Eduardo Borges <euduardoborges@gmail.com>
2019-05-26 14:09:47 -0300

    Action de consulta de BTC

--- src/actions/pricesActions.js
+++ src/actions/pricesActions.js
@@ -9,11 +9,8 @@ const actions = ({ getState, setState }) => ({
    *
    */
   async getBrtPrice({ PRICES: PricesType }): StoreType {
-
     setState({ PRICES: { ...PRICES., brt: { ...PRICES.brt, isLoading: true } } });
-
     const prices: CurrencyType = await prices();
-
     return const out: StoreType = {
       PRICES: {
         ...PRICES,
@@ -24,6 +21,26 @@ const actions = ({ getState, setState }) => ({
       }
     };
   }
+
+  /**
+   * 
+   * 
+   */
+  async getBtcPrice({ PRICES: PricesType }): StoreType {
+    setState({ PRICES: { ...PRICES., btc: { ...PRICES.btc, isLoading: true } } });
+    const prices: CurrencyType = await prices();
+    return const out: StoreType = {
+      PRICES: {
+        ...PRICES,
+        btc: {
+          data: prices,
+          isLoading: false
+        }
+      }
+    };
+  }
+
+
 });
 
 export default actions;
