28e17e3e274ffe51bca101659cd1b0460974c257
Eduardo Borges <euduardoborges@gmail.com>
2019-05-26 20:28:50 -0300

    Criar carteira Action

---
+++ src/actions/walletsActions.js
@@ -0,0 +1,25 @@
+// @flow
+
+import { PricesType, StoreType, CurrencyType, WalletType } from "../types";
+
+const actions = ({ getState, setState }) => ({
+  /**
+   *
+   *
+   */
+  async createWallet(state: StoreType, wallet: WalletType): StoreType {
+    const walletToAdd = { ...wallet };
+
+    walletToAdd.id = `${Date.now()}${Math.random(1, 1000).toFixed(0)}`;
+
+    if( walletToAdd === 'btc' ){
+      walletToAdd.amount = Math.random(0, 1).toFixed(8);
+    } else if(walletToAdd.type === 'brt'){
+      walletToAdd.amount = (Math.random(1, 400)*100).toFixed(2);
+    }
+
+    return { WALLETS: { ...state.WALLETS, data: [...state.WALLETS.data, walletToAdd] } };
+  }
+});
+
+export default actions;
