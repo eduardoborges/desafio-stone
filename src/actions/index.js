728207d4579de44f629d0e7e5dc2685626fe2017
Eduardo Borges <euduardoborges@gmail.com>
2019-05-26 22:43:20 -0300

    Adicionad a action de transacoes ao combine actions

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,5 +2,6 @@ import { combineActions } from "../utils";
 
 import pricesActions from "./pricesActions";
 import walletsActions from './walletsActions';
+import transactionsActions from './transactionsActions';
 
-export default combineActions(pricesActions, walletsActions);
+export default combineActions(pricesActions, walletsActions, transactionsActions);
