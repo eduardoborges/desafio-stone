897a976f6c3d60985eaba20f13f8fdef05fd9a43
Eduardo Borges <euduardoborges@gmail.com>
2019-05-26 22:43:43 -0300

    Correcoes na tela de Nova Carteira

--- src/screens/wallet-create/index.js
+++ src/screens/wallet-create/index.js
@@ -5,12 +5,15 @@ import actions from "../../actions";
 
 function WalletCreateScreen(props) {
   const blankForm: WalletType = {
-    name: "Teste",
-    type: "btc"
+    name: "Carteira",
+    type: null
   };
   const [form, setForm] = useState(blankForm);
 
-  const handleSubmit = () => props.createWallet(form);
+  const handleSubmit = (e) =>{
+    e.preventDefault();
+    props.createWallet(form)
+  };
 
   const close = () => props.history.push("/wallets");
 
@@ -18,42 +21,47 @@ function WalletCreateScreen(props) {
     <div class="modal is-active">
       <div class="modal-background" onClick={close} />
       <div class="modal-card">
-        <header class="modal-card-head">
-          <p class="modal-card-title">Nova Carteira</p>
-          <button class="delete" aria-label="close" onClick={close} />
-        </header>
-        <section class="modal-card-body">
-          <div className="columns is-multiline">
-            <div className="column is-6">
-              <input
-                type="text"
-                className="input"
-                value={form.name}
-                onChange={e => setForm({ ...form, name: e.target.value })}
-              />
-            </div>
-            <div className="column is-6">
-              <div class="select is-primary is-fullwidth">
-                <select
-                  value={form.type}
-                  defaultValue={form.type}
-                  onChange={e => setForm({ ...form, type: e.target.value })}
-                >
-                  <option value="btc">BTC</option>
-                  <option value="brt">BRT</option>
-                </select>
+        <form onSubmit={handleSubmit}>
+          <header class="modal-card-head">
+            <p class="modal-card-title">Nova Carteira</p>
+            <button class="delete" aria-label="close" onClick={close} />
+          </header>
+          <section class="modal-card-body">
+
+            <div className="columns is-multiline">
+              <div className="column is-6">
+                <input
+                  type="text"
+                  className="input"
+                  value={form.name}
+                  onChange={e => setForm({ ...form, name: e.target.value })}
+                />
+              </div>
+              <div className="column is-6">
+                <div class="select is-fullwidth">
+                  <select
+                    value={form.type}
+                    defaultValue={form.type}
+                    onChange={e => setForm({ ...form, type: e.target.value })}
+                    required
+                  >
+                    <option disabled selected>Escolhe uma...</option>
+                    <option value="btc">BTC</option>
+                    <option value="brt">BRT</option>
+                  </select>
+                </div>
               </div>
             </div>
-          </div>
-        </section>
-        <footer class="modal-card-foot">
-          <button onClick={handleSubmit} class="button is-success">
-            Criar nova carteira
-          </button>
-          <button class="button" onClick={close}>
-            Cancelar
-          </button>
-        </footer>
+          </section>
+          <footer class="modal-card-foot">
+            <button type="submit" class="button is-success">
+              Criar nova carteira
+            </button>
+            <button class="button" onClick={close}>
+              Cancelar
+            </button>
+          </footer>
+        </form>
       </div>
     </div>
   );
