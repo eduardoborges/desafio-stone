de6f392086b5d9ed959d98a19dd7f4d17a3dd75f
Eduardo Borges <euduardoborges@gmail.com>
2019-05-26 22:43:02 -0300

    Funcao auxiliar para gerar unique id

--- src/utils.js
+++ src/utils.js
@@ -8,3 +8,10 @@
 export function combineActions(...fns) {
   return (...args) => Object.assign(...fns.map(fn => fn(...args)));
 };
+
+
+// 
+
+export function uniq(){
+  return Number(`${Date.now()}${(Math.random(1,1000)*100).toFixed(0)}`);
+}
