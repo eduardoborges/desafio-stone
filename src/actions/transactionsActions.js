7aae1c14bfb34246b2c41a583d3da05c9aee4033
Eduardo Borges <euduardoborges@gmail.com>
2019-05-27 20:43:19 -0300

    Fazer as alteracoes pertinentes nas carteiras apos transacoes

--- src/actions/transactionsActions.js
+++ src/actions/transactionsActions.js
@@ -9,13 +9,30 @@ const actions = ({ getState, setState }) => ({
    */
   async runSellTransaction(state: StoreType, payload: TransactionType): StoreType {
     let transaction = { ...payload };
+    let wallets = [...state.WALLETS.data];
+
+    console.log(transaction.finalAmount)
+
+    wallets = wallets.map(w => {
+      let wallet = { ...w };
+      if (wallet.id === transaction.walletSource) {
+        wallet.amount = Number(wallet.amount) - Number(transaction.amount);
+      }
+      if (wallet.id === transaction.walletDestination) {
+        wallet.amount = Number(wallet.amount) + Number(transaction.finalAmount);
+      }
+      return wallet;
+    });
 
     transaction.id = uniq();
     transaction.date = new Date();
 
-    return {
+    const out = {
+      WALLETS: { ...state.WALLETS, data: [...wallets] },
       TRANSACTIONS: { ...state.TRANSACTIONS, data: [...state.TRANSACTIONS.data, transaction] }
     };
+
+    return out;
   }
 });
 
