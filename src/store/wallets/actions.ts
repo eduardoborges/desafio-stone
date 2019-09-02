331e79bdf81b3db75d97aa158303a11315979958
Eduardo Borges <euduardoborges@gmail.com>
2019-09-02 18:45:03 -0300

    (todo): new wallet screen

--- src/store/wallets/actions.ts
+++ src/store/wallets/actions.ts
@@ -2,17 +2,21 @@
 
 import { Store, ActionCreator } from 'unistore';
 import { navigate } from '@reach/router';
+import { toast } from 'react-toastify';
 import { AppState } from '../index';
 import { WalletState, Wallet } from './types';
 
 const actions = (store:Store<AppState>) => ({
 
-  createWallet: async ({ WALLETS }: AppState, payload: Wallet) => ({
-    WALLETS: {
-      data: [...WALLETS.data, { id: Date.now(), ...payload }],
-      isLoading: false,
-    },
-  }),
+  createWallet: async ({ WALLETS }: AppState, payload: Wallet) => {
+    toast('Carteira criada.', { type: toast.TYPE.SUCCESS });
+    return ({
+      WALLETS: {
+        data: [...WALLETS.data, { id: Date.now(), ...payload }],
+        isLoading: false,
+      },
+    });
+  },
 
   removeWallet: ({ WALLETS }: AppState, wallet: Wallet) => ({
     WALLETS: {
