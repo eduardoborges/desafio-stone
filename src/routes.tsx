ec3a647f857b90502d47869569559bf10e6f62c1
Eduardo Borges <euduardoborges@gmail.com>
2019-09-02 18:45:56 -0300

    (todo): add new wallet to route file

--- src/routes.tsx
+++ src/routes.tsx
@@ -4,6 +4,7 @@ import { Router, Redirect } from '@reach/router';
 import Login from './screens/login';
 import Exchange from './screens/exchange';
 import Wallets from './screens/exchange/wallets';
+import NewWallet from './screens/exchange/new-wallet';
 
 export default () => (
   <Router>
@@ -11,6 +12,7 @@ export default () => (
     <Login path="/login" />
     <Exchange path="/exchange">
       <Wallets path="wallets" />
+      <NewWallet path="wallets/new" />
     </Exchange>
   </Router>
 );
