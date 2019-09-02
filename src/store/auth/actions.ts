4c6294c3a52e193c19998b072035055a52471a2b
Eduardo Borges <euduardoborges@gmail.com>
2019-09-02 16:55:05 -0300

    (todo): simple auth actions

---
+++ src/store/auth/actions.ts
@@ -0,0 +1,36 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+
+import { Store, ActionCreator } from 'unistore';
+import { AppState } from '../index';
+import { AuthState } from './types';
+
+const actions = (store:Store<AppState>) => ({
+
+  handleLogin: async (state: AppState) => {
+    setTimeout(() => {
+      store.setState({ AUTH: { ...state.AUTH, isAuth: true, isLoading: false } });
+    }, 2000); // :P
+  },
+
+  handleCheckLogin: async (state:AppState) => {
+    if (state.AUTH.isLoading) return;
+    store.setState({ AUTH: { ...state.AUTH, isLoading: true } });
+    setTimeout(() => {
+      store.setState({ AUTH: { ...state.AUTH, isAuth: true, isLoading: false } });
+    }, 2000); // :P
+  },
+
+  handleLogout: () => ({
+    AUTH: {
+      isAuth: false,
+      isLoading: false,
+    },
+  }),
+
+  handleRefreshToken: () => {
+    // DUMP
+  },
+});
+
+
+export default actions;
