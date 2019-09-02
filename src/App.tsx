28f568cd630e64b2d9f1b60d4215d9792a8c9f6d
Eduardo Borges <euduardoborges@gmail.com>
2019-09-02 18:42:24 -0300

    (dep): toastr

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import { hot } from 'react-hot-loader/root';
 import React from 'react';
+import { ToastContainer } from 'react-toastify';
 import { Provider } from 'unistore/react';
 import store from './store';
 import Routes from './routes';
 
 const App = () => (
   <Provider store={store}>
-    <Routes />
+    <>
+      <Routes />
+      <ToastContainer />
+    </>
   </Provider>
 );
 
