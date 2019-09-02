dee1cf5aedecefc0d2bd5435ae708905e842dd11
Eduardo Borges <euduardoborges@gmail.com>
2019-09-02 18:45:37 -0300

    (todo): add wallets to initial state

--- src/store/index.ts
+++ src/store/index.ts
@@ -2,15 +2,21 @@ import createStore from 'unistore';
 import devtools from 'unistore/devtools';
 
 import { AuthState } from './auth/types';
+import { WalletState } from './wallets/types';
 
 export interface AppState {
   AUTH: AuthState,
+  WALLETS: WalletState,
 }
 
 const initialState: AppState = {
   AUTH: {
     isAuth: false,
     isLoading: true,
+  },
+  WALLETS: {
+    data: [],
+    isLoading: false,
   },
 };
 
