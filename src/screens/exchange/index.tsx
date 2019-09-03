58e99591c5a667618352481021df6f4d99474d07
Eduardo Borges <euduardoborges@gmail.com>
2019-09-03 17:26:42 -0300

    (fix): minor fixes

--- src/screens/exchange/index.tsx
+++ src/screens/exchange/index.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import React, { useEffect } from 'react';
 import { RouteComponentProps } from '@reach/router';
 import { connect } from 'unistore/react';
