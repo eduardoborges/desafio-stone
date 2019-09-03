49c218a11c61f269c9f57663b716f2f814c85616
Eduardo Borges <euduardoborges@gmail.com>
2019-09-03 17:26:54 -0300

    (todo): add new transaction to routes

--- src/routes.tsx
+++ src/routes.tsx
@@ -5,14 +5,17 @@ import Login from './screens/login';
 import Exchange from './screens/exchange';
 import Wallets from './screens/exchange/wallets';
 import NewWallet from './screens/exchange/new-wallet';
+import NewTransaction from './screens/exchange/new-transaction';
 
 export default () => (
   <Router>
     <Redirect from="/" to="/login" />
     <Login path="/login" />
     <Exchange path="/exchange">
-      <Wallets path="wallets" />
-      <NewWallet path="wallets/new" />
+      <Wallets path="wallets">
+        <NewWallet path="new" />
+        <NewTransaction path=":id/transaction" />
+      </Wallets>
     </Exchange>
   </Router>
 );
