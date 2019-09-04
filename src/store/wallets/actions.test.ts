4a5126dc171d992e0eee6f8053e498d306433f58
Eduardo Borges <euduardoborges@gmail.com>
2019-09-04 18:27:53 -0300

    (tests): create wallet - blank fields test

--- src/store/wallets/actions.test.ts
+++ src/store/wallets/actions.test.ts
@@ -2,6 +2,7 @@ import store from 'store';
 import actions from './actions';
 
 describe('Wallets Actions Tests', () => {
+  const initialState = store.getState();
   const initialAmount = 10000;
 
   const wallet1 = {
@@ -13,9 +14,10 @@ describe('Wallets Actions Tests', () => {
   };
 
 
-  it('Create new Wallet', async () => {
-    const { createWallet } = actions(store);
+  it('Create new wallet success', async () => {
+    store.setState(initialState);
 
+    const { createWallet } = actions(store);
     // @ts-ignore
     await store.setState(await createWallet(store.getState(), wallet1));
     // @ts-ignore
@@ -25,9 +27,10 @@ describe('Wallets Actions Tests', () => {
   });
 
 
-  it('Remove Wallet', async () => {
-    const { createWallet, removeWallet } = actions(store);
+  it('Remove wallet success', async () => {
+    store.setState(initialState);
 
+    const { createWallet, removeWallet } = actions(store);
     store.setState({ WALLETS: { ...store.getState().WALLETS, data: [] } });
     // @ts-ignore
     store.setState(await createWallet(await store.getState(), wallet1));
@@ -42,5 +45,16 @@ describe('Wallets Actions Tests', () => {
     await store.setState(removeWallet(store.getState(), wallet2));
 
     expect(await store.getState().WALLETS.data).toHaveLength(0);
+  });
+
+  it('Dont allow blank field', async () => {
+    store.setState(initialState);
+    const { createWallet, removeWallet } = actions(store);
+
+    const wallet = { amount: 10000 };
+    // @ts-ignore
+    store.setState(await createWallet(await store.getState(), wallet));
+
+    expect(store.getState().WALLETS.data).toHaveLength(0);
   });
 });
--- src/store/wallets/actions.ts
+++ src/store/wallets/actions.ts
@@ -3,19 +3,31 @@
 import { Store, ActionCreator } from 'unistore';
 import { navigate } from '@reach/router';
 import { toast } from 'react-toastify';
+import * as Yup from 'yup';
 import { AppState } from '../index';
 import { WalletState, Wallet } from './types';
+
 
 const actions = (store:Store<AppState>) => ({
 
   createWallet: async ({ WALLETS }: AppState, payload: Wallet) => {
-    toast('Carteira criada.', { type: toast.TYPE.SUCCESS });
-    return ({
-      WALLETS: {
-        data: [...WALLETS.data, { id: Date.now(), amount: 10000, ...payload }],
-        isLoading: false,
-      },
+    const schema = Yup.object().shape({
+      id: Yup.number(),
+      name: Yup.string().min(4).required(),
+      amout: Yup.number(),
+      type: Yup.string().oneOf(['BTC', 'BRT']).required(),
     });
+
+    if (schema.isValidSync(payload)) {
+      toast('Carteira criada.', { type: toast.TYPE.SUCCESS });
+      return ({
+        WALLETS: {
+          data: [...WALLETS.data, { id: Date.now(), amount: 10000, ...payload }],
+          isLoading: false,
+        },
+      });
+    }
+    return { WALLETS };
   },
 
   removeWallet: ({ WALLETS }: AppState, wallet: Wallet) => ({
