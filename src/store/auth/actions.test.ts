3a62ce0ff9dd4db811b48c16ea802550d6102ac6
Eduardo Borges <euduardoborges@gmail.com>
2019-09-04 13:57:36 -0300

    (test): auth check login

--- src/store/auth/actions.test.ts
+++ src/store/auth/actions.test.ts
@@ -25,4 +25,23 @@ describe('Auth Actions Tests', () => {
     await store.setState(resp);
     expect(store.getState().AUTH.isAuth).toBe(false);
   });
+
+  it('Handle Check Login', async () => {
+    const { handleCheckLogin } = actions(store);
+    const state = store.getState();
+
+    await store.setState({ AUTH: { isAuth: false, isLoading: false } }); // nao autenticado
+
+    // @ts-ignore
+    await store.action(handleCheckLogin(state));
+    await sleep(2000);
+    expect(store.getState().AUTH.isAuth).toBeTruthy();
+
+    await store.setState({ AUTH: { isAuth: true, isLoading: false } }); //  autenticado
+
+    // @ts-ignore
+    await store.action(handleCheckLogin(state));
+    await sleep(2000);
+    expect(store.getState().AUTH.isAuth).toBeTruthy();
+  });
 });
