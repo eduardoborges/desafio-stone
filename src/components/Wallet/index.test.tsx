93644009e3dbbadaa6275e17e6f513e029c15764
Eduardo Borges <euduardoborges@gmail.com>
2019-09-04 17:53:38 -0300

    (tests): wallet component test

---
+++ src/components/Wallet/index.test.tsx
@@ -0,0 +1,17 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import React from 'react';
+import { shallow, render, mount } from 'enzyme';
+
+import Wallet from './index';
+
+describe('Title Component', () => {
+  it('Must be render childreen sucessfuly', () => {
+    const el = mount(
+      <Wallet name="Teste" type="BRT" id={1} amount={1000}>
+        Teste
+      </Wallet>,
+    );
+    expect(el).toBeTruthy();
+    el.unmount();
+  });
+});
